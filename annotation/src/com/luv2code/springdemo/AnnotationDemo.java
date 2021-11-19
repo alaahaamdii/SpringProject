bb264bda58e67dc2e23a0f97a73b9b3195c5505c
Alaa Haamdi <alaahaamdi@spectrumgroupe.fr>
2021-11-19 09:26:10 +0000

    properties using annotation

--- annotation/src/com/luv2code/springdemo/AnnotationDemo.java
+++ annotation/src/com/luv2code/springdemo/AnnotationDemo.java
@@ -6,23 +6,32 @@ public class AnnotationDemo {
 
 	public static void main(String[] args) {
 
-		// read spring config file
+		// load the spring configuration file
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+				
 		// get the bean from spring container
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
-
-		// call method to get daily fortune
-		System.out.println(theCoach.getDailyFortune());
 				
+		// call method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
+			
+		// call our new swim coach methods ... has the props values injected
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
+		
 		// close the context
-		context.close();	
+		context.close();
 	}
 
 }
+
+
+
+
+
 
 
